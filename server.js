c7a49e49dcc8d5014eb622414d91014ab96e6d61
dread <dreadcreator5@gmail.com>
2023-09-13 11:42:28 +0530

    add express routes
    we are now using express and node so using routes are better instead of
    href in html
    
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can choose any available port
+const port = 3000;
 
-app.use(express.static('public')); // Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Route to the home page (index.html)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Route to the about page (about.html)
+app.get('/random', (req, res) => {
+  res.sendFile(__dirname + '/public/pages/random/index.html');
+});
+
+// Route to the contact page (contact.html)
+app.get('/image-gen', (req, res) => {
+  res.sendFile(__dirname + '/public/pages/gloryv2/index.html');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
