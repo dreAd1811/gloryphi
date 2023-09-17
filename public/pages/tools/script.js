e33f3e6ee38a55c22bbf77e5bb07134af32ec930
dread <dreadcreator5@gmail.com>
2023-09-17 19:40:21 +0530

    TEST: test some path for api
    idk in vercel api isnt found so maybe path is messd up???????
    
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- public/pages/tools/script.js
+++ public/pages/tools/script.js
@@ -30,49 +30,49 @@ function shortenURL(longURL) {
 // Fraud Check CODE
 
 // Function to get the user's IP address
-async function getUserIP() {
-    try {
-      const response = await fetch('https://api.ipify.org?format=json');
-      if (response.ok) {
-        const data = await response.json();
-        return data.ip;
-      } else {
-        console.error('Failed to fetch user IP.');
+    async function getUserIP() {
+      try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        if (response.ok) {
+          const data = await response.json();
+          return data.ip;
+        } else {
+          console.error('Failed to fetch user IP.');
+          return null;
+        }
+      } catch (error) {
+        console.error('Error:', error.message);
         return null;
       }
-    } catch (error) {
-      console.error('Error:', error.message);
-      return null;
     }
-  }
 
-  // Function to fetch data from the server and display it
-  async function fetchData() {
-    try {
-      const userIP = await getUserIP(); // Get the user's IP address
-      if (!userIP) {
-        console.error('User IP not available.');
-        return;
+    // Function to fetch data from the server and display it
+    async function fetchData() {
+      try {
+        const userIP = await getUserIP(); // Get the user's IP address
+        if (!userIP) {
+          console.error('User IP not available.');
+          return;
+        }
+        console.log(userIP)
+        const response = await fetch(`/scrape/${userIP}`); // Send a request to the server with the user's IP
+
+        // Check if the request was successful (status code 200)
+        if (response.ok) {
+          const data = await response.json(); // Parse the JSON response
+          const scoreValue = data.score; // Extract the "score" value
+
+          // Set the "scoreValue" as the text of the <p> element with class "res"
+          document.querySelector('.res').textContent = scoreValue;
+
+        } else {
+          console.error('Failed to fetch data. Status code:', response.status);
+        }
+      } catch (error) {
+        console.error('Error:', error.message);
       }
-      console.log(userIP)
-      const response = await fetch(`/scrape/${userIP}`); // Send a request to the server with the user's IP
-
-      // Check if the request was successful (status code 200)
-      if (response.ok) {
-        const data = await response.json(); // Parse the JSON response
-        const scoreValue = data.score; // Extract the "score" value
-
-        // Set the "scoreValue" as the text of the <p> element with class "res"
-        document.querySelector('.res').textContent = scoreValue;
-
-      } else {
-        console.error('Failed to fetch data. Status code:', response.status);
-      }
-    } catch (error) {
-      console.error('Error:', error.message);
     }
-  }
 
-  // Attach click event listener to the button
-  document.getElementById('scrapeButton').addEventListener('click', fetchData);
+    // Attach click event listener to the button
+    document.getElementById('scrapeButton').addEventListener('click', fetchData);
 
