d490e58ff8e3d7586155cd249c7f6fce974041b7
dread <dreadcreator5@gmail.com>
2023-09-18 08:48:21 +0530

    ip: make it more accurate
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- api/saveIpAddress.js
+++ api/saveIpAddress.js
@@ -1,8 +1,11 @@
 // api/saveIpAddress.js
 module.exports = (req, res) => {
-    const userIpAddress = req.headers['x-real-ip'] || req.connection.remoteAddress;
+    const forwardedFor = req.headers['x-forwarded-for'];
+    const realIp = req.headers['x-real-ip'];
+    const clientIp = forwardedFor || realIp || req.connection.remoteAddress;
+  
     // Save the IP address to a database or perform any other desired action.
-    console.log(`Received IP address: ${userIpAddress}`);
+    console.log(`Received IP address: ${clientIp}`);
     res.status(200).send('IP address saved successfully');
   };
   
